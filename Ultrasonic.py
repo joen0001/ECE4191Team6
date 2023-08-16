79d7cd69d03c56b2a318b46844b574cbde8bc565
joen0001 <61935138+joen0001@users.noreply.github.com>
2023-08-16 11:22:39 +1000

    Create Ultrasonic.py

---
+++ Ultrasonic.py
@@ -0,0 +1,8 @@
+from CONFIG.py import *  
+from gpiozero import DistanceSensor
+
+def Ultrasonic():
+  sensor_front = DistanceSensor(ULT_FRONT_ECHO, ULT_FRONT_TRIG)
+	sensor_right = DistanceSensor(ULT_RIGHT_ECHO, ULT_RIGHT_TRIG)
+	sensor_left = DistanceSensor(ULT_LEFT_ECHO, ULT_LEFT_TRIG)
+  return sensor_front.distance*100, sensor_right.distance*100, sensor_left.distance*100
