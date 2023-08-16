f86498b61c15d64c61644fd68ae7559041b891c4
joen0001 <joen0001@student.monash.edu>
2023-08-16 13:29:56 +1000

    code updates
    idk if any of this will actually work
    starting skeleton code

--- main.py
+++ main.py
@@ -1,52 +1,17 @@
 from CONFIG import *
 from Ultrasonic import Ultrasonic
-from Multiprocessing import Process, Value
-from gpiozero import Motor, RotaryEncoder
+from ShaftEncoder import ShaftEncoder
+import numpy as np
+from Multiprocessing import Process, Value, Array
 
-class DiffDriveRobot:  
-    def __init__(self,inertia=5, dt=0.1, drag=0.2, wheel_radius=WHEEL_RADIUS, wheel_sep=WHEEL_SEPARATION):
-        
-        self.x = 0.0 # y-position
-        self.y = 0.0 # y-position 
-        self.th = 0.0 # orientation
-        
-        self.wl = 0.0 #rotational velocity left wheel
-        self.wr = 0.0 #rotational velocity right wheel
-        
-        self.I = inertia
-        self.d = drag
-        self.dt = dt
-        
-        self.r = wheel_radius
-        self.l = wheel_sep
-    
-    # Motor encoder measurement which measures how fast wheel is turning
-  # NEED TO COMPLETE
-    def RotEnc(A, B):
-      encoder = RotaryEncoder(A, B, max_steps=10000)
-      #encoder.steps
-      return w
-    
-    # Veclocity motion model
-    def base_velocity(self,wl,wr):
-        
-        v = (wl*self.r + wr*self.r)/2.0
-        
-        w = (wl*self.r - wr*self.r)/self.l
-        
-        return v, w
-    
-    # Kinematic motion model
-    def pose_update(self,duty_cycle_l,duty_cycle_r):
-        
-        self.wl = self.motor_simulator(self.wl,duty_cycle_l)
-        self.wr = self.motor_simulator(self.wr,duty_cycle_r)
-        
-        v, w = self.base_velocity(self.wl,self.wr)
-        
-        self.x = self.x + self.dt*v*np.cos(self.th)
-        self.y = self.y + self.dt*v*np.sin(self.th)
-        self.th = self.th + w*self.dt
-        
-        return self.x, self.y, self.th
-        
+def main():
+
+  ultra_arr = Array('i',[0,0,0]) # 'i' for signed integer, 'd' for double prec float
+
+  TH_General = Process(target = )
+  TH_Ultrasonic = Process(target = Ultrasonic, args=(ultra_arr))
+  TH_ShaftEncoder = Process(target = ShaftEncoder)
+
+  TH_General.start()
+  TH_Ultrasonic.start()
+  TH_ShaftEncoder.start()
