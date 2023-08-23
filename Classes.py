f833d1740a494f039b5fae76e5492e9805720458
travisbgithub <142949398+travisbgithub@users.noreply.github.com>
2023-08-23 10:03:13 +1000

    hi

--- Classes.py
+++ Classes.py
@@ -1,5 +1,11 @@
 import gpiozero
-
+from CONFIG import *
+from Ultrasonic import Ultrasonic
+from ShaftEncoder import ShaftEncoder
+import numpy as np
+from Multiprocessing import Process, Value, Array
+from gpiozero import Motor, RotaryEncoder
+from matplotlib import pyplot as plt
 
 class Motor:
     def __init__(self, forward_pin, backward_pin, enable_pin):
@@ -50,3 +56,124 @@ class Motor:
         :return: A boolean representing if the motor is active
         """
         return self.enable_pwm.value > 0
+
+class DiffDriveRobot:  
+    def __init__(self,inertia=5, dt=0.1, drag=0.2, wheel_radius=WHEEL_RADIUS, wheel_sep=WHEEL_SEPARATION):
+        
+        self.x = 0.0 # y-position
+        self.y = 0.0 # y-position 
+        self.th = 0.0 # orientation
+        
+        self.wl = 0.0 #rotational velocity left wheel
+        self.wr = 0.0 #rotational velocity right wheel
+        
+        self.I = inertia
+        self.d = drag
+        self.dt = dt
+        
+        self.r = wheel_radius
+        self.l = wheel_sep
+    
+    # Motor encoder measurement which measures how fast wheel is turning
+  # NEED TO COMPLETE
+    def RotEnc(A, B):
+      encoder = RotaryEncoder(A, B, max_steps=10000)
+      #encoder.steps
+      return w
+    
+    # Veclocity motion model
+    def base_velocity(self,wl,wr):
+        
+        v = (wl*self.r + wr*self.r)/2.0
+        
+        w = (wl*self.r - wr*self.r)/self.l
+        
+        return v, w
+    
+    # Kinematic motion model
+    def pose_update(self,duty_cycle_l,duty_cycle_r):
+        
+        self.wl = self.motor_simulator(self.wl,duty_cycle_l)
+        self.wr = self.motor_simulator(self.wr,duty_cycle_r)
+        
+        v, w = self.base_velocity(self.wl,self.wr)
+        
+        self.x = self.x + self.dt*v*np.cos(self.th)
+        self.y = self.y + self.dt*v*np.sin(self.th)
+        self.th = self.th + w*self.dt
+        
+        return self.x, self.y, self.th
+    
+    
+        
+class TentaclePlanner:
+    
+    def __init__(self,dt=0.1,steps=5,alpha=1,beta=0.1):
+        
+        self.dt = dt
+        self.steps = steps
+        # Tentacles are possible trajectories to follow
+        #self.tentacles = [(0.0,0.5),(0.0,-0.5),(0.1,1.0),(0.1,-1.0),(0.1,0.5),(0.1,-0.5),(0.1,0.0),(0.0,0.0)]
+        
+        self.alpha = alpha
+        self.beta = beta
+        self.avoid_left = False
+        self.left_counter = 0
+    
+    # Play a trajectory and evaluate where you'd end up
+    def roll_out(self,v,w,goal_x,goal_y,goal_th,x,y,th):
+        
+        for j in range(self.steps):
+        
+            x = x + self.dt*v*np.cos(th)
+            y = y + self.dt*v*np.sin(th)
+            th = (th + w*self.dt)
+        
+        e_th = goal_th-th
+        e_th = np.arctan2(np.sin(e_th),np.cos(e_th))
+        
+        return self.alpha*((goal_x-x)**2 + (goal_y-y)**2) + self.beta*(e_th**2)
+        
+    # Choose trajectory that will get you closest to the goal
+    def plan(self,goal_x,goal_y,goal_th,x,y,th):
+        
+        front_sensor, left_sensor, right_sensor = Ultrasonic()
+        costs =[]
+        for v,w in self.tentacles:
+            costs.append(self.roll_out(v,w,goal_x,goal_y,goal_th,x,y,th))
+        
+        best_idx = np.argmin(costs)
+        
+        return self.tentacles[best_idx]
+
+
+class RobotController:
+    
+    def __init__(self,Kp=0.1,Ki=0.01,wheel_radius=0.02, wheel_sep=0.1):
+        
+        self.Kp = Kp
+        self.Ki = Ki
+        self.r = wheel_radius
+        self.l = wheel_sep
+        self.e_sum_l = 0
+        self.e_sum_r = 0
+        
+    def p_control(self,w_desired,w_measured,e_sum):
+        
+        duty_cycle = min(max(-1,self.Kp*(w_desired-w_measured) + self.Ki*e_sum),1)
+        
+        e_sum = e_sum + (w_desired-w_measured)
+        
+        return duty_cycle, e_sum
+        
+        
+    def drive(self,v_desired,w_desired,wl,wr):
+        
+        wl_desired = (v_desired + self.l*w_desired/2)/self.r
+        wr_desired = (v_desired - self.l*w_desired/2)/self.r
+        
+        duty_cycle_l,self.e_sum_l = self.p_control(wl_desired,wl,self.e_sum_l)
+        duty_cycle_r,self.e_sum_r = self.p_control(wr_desired,wr,self.e_sum_r)
+        
+        return duty_cycle_l, duty_cycle_r
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Ultrasonic import Ultrasonic
 from ShaftEncoder import ShaftEncoder
 import numpy as np
 from Multiprocessing import Process, Value, Array
+from gpiozero import Motor, RotaryEncoder
+from matplotlib import pyplot as plt
 
 def main():
 
@@ -15,3 +17,31 @@ def main():
   TH_General.start()
   TH_Ultrasonic.start()
   TH_ShaftEncoder.start()
+  robot = DiffDriveRobot(inertia=10, dt=0.1, drag=2, wheel_radius=0.05, wheel_sep=0.15)
+  controller = RobotController(Kp=1.0,Ki=0.15,wheel_radius=0.05,wheel_sep=0.15)
+  planner = TentaclePlanner(dt=0.1,steps=5,alpha=1,beta=1e-5)
+
+
+  poses = []
+  velocities = []
+  duty_cycle_commands = []
+
+  goal_x = 2*np.random.rand()-1 #NEEDS CHANGING
+  goal_y = 2*np.random.rand()-1 #NEEDS CHANGING
+  goal_th = 2*np.pi*np.random.rand()-np.pi #NEEDS CHANGING
+
+  for i in range(200):
+
+      # Plan using tentacles
+      v,w = planner.plan(goal_x,goal_y,goal_th,robot.x,robot.y,robot.th)
+        
+      duty_cycle_l,duty_cycle_r = controller.drive(v,w,robot.wl,robot.wr) 
+        
+      # Simulate robot motion - send duty cycle command to robot
+      x,y,th = robot.pose_update(duty_cycle_l,duty_cycle_r)
+        
+      # Log data
+      poses.append([x,y,th])
+      duty_cycle_commands.append([duty_cycle_l,duty_cycle_r])
+      velocities.append([robot.wl,robot.wr])
+
