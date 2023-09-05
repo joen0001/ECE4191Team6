b56e76fd08a191ee3e026222ae206ecc2ad06ea0
Raspberry Pi <jeremy.rabinowicz@gmail.com>
2023-09-05 04:39:18 +0000

    Tested something with ultrasonic

--- test_ultrasonic.py
+++ test_ultrasonic.py
@@ -1,18 +1,41 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor_l = DistanceSensor(echo=1, trigger=7)
+class Ultra:
+    counter_max = 10
+    def __init__(self, echo, trigger) -> None:
+        self.sensor = DistanceSensor(echo, trigger)
+        self.old_val = 0
+        self.counter = 0
+
+    def distance(self):
+        val = self.sensor.distance
+        if val == self.old_val and self.counter >= Ultra.counter_max:
+            return 1
+        elif val == self.old_val:
+            self.counter += 1
+        else:
+            self.old_val = val
+            self.counter = 0
+        return val
+
+sensor_l = Ultra(echo=1, trigger=7)
 sensor_m = DistanceSensor(echo=9, trigger=10)
 sensor_r = DistanceSensor(echo=26, trigger=16)
 sensor_new_m = DistanceSensor(echo=15, trigger = 14)
 while True:
-    print('Distance_L: ', sensor_l.distance * 100)
+    print('Distance_L: ', sensor_l.distance() * 100)
     print('Distance_M: ', sensor_m.distance * 100)
     print('Distance_R: ', sensor_r.distance * 100)
     print('Distance_M_new: ', sensor_new_m.distance * 100)
     sleep(0.1)
     
 
+
+
+
+      
+   
 
 #RIGHT ECHO=26, TRIG=16
 #MID ECHO=9 TRIG=10
