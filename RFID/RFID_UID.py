ae63ba20d1c91bd019be20d55b22e72f9e9e0454
joen-01 <joen0001@student.monash.edu>
2023-09-13 13:18:43 +1000

    Create RFID_UID.py

---
+++ RFID/RFID_UID.py
@@ -0,0 +1,36 @@
+from mrfc522 import BasicMFRC522
+reader = BasicMFRC522()
+
+A1 = 584193570396
+A2 = 584193308248
+A3 = 584195405368
+A4 = 584195667516
+
+B1 = 584191014023
+B2 = 584191276155
+B3 = 584188458401
+B4 = 584188720549
+
+C1 = 584196650284
+C2 = 584196388136
+C3 = 584183936490
+C4 = 584184198638
+
+List_A = [A1,A2,A3,A4]
+List_B = [B1,B2,B3,B4]
+List_C = [C1,C2,C3,C4]
+
+Loc_Ind = ["A","B","C"]
+
+List = [List_A,List_B,List_C]
+
+id = reader.read_id()
+
+for letter in range(3):
+    for ind in range(4):
+        if id == List[letter][ind]:
+             Loc = Loc_Ind[letter]
+             Loc_Id = ind
+
+print("LOCATION: " + Loc)
+print("ID:" + str(Loc_Id))
