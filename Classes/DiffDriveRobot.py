675817543ae0fbe7454d01ee83271c42034a866d
Raspberry Pi <jeremy.rabinowicz@gmail.com>
2023-10-16 09:14:02 +0000

    Revert "THIS IS WORKING THIS IS WORKING FALLBACK IF NEEDED"
    This reverts commit 802d2a2809ebca5587f24b94c94771d6a1b0aac9.

--- Classes/DiffDriveRobot.py
+++ Classes/DiffDriveRobot.py
@@ -35,6 +35,7 @@ class DiffDriveRobot:
         self.y = self.y + (end-start)*v*np.sin(self.th)
         self.th = self.th + w*self.dt
         start = time.process_time()
+        print(f'x: {self.x}' f'y: {self.y}' f'th: {self.th}'f'start: {start}'f'end: {end}')
               
         return self.x, self.y, self.th,start
         '''
